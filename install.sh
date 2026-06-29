a8216dfe15521751be89d99bb54774536033dc4c
chrispyware <thegreatchrispy@gmail.com>
2026-06-29 00:47:12 -0500

    Restructure workstation repo and add bootstrap script

--- install.sh
+++ install.sh
@@ -16,10 +16,13 @@ echo
 mkdir -p ~/.config
 
 echo "Installing .zshrc..."
-cp "$DOTFILES_DIR/home/.zshrc" ~/.zshrc
+cp "$DOTFILES_DIR/dotfiles/home/.zshrc" ~/.zshrc
+
+echo "Installing i3 config..."
+cp "$DOTFILES_DIR/dotfiles/config/i3/config" ~/.config/i3/config
 
 echo "Installing Chrispyware config..."
-cp -r "$DOTFILES_DIR/config/chrispyware" ~/.config/
+cp -r "$DOTFILES_DIR/dotfiles/config/chrispyware" ~/.config/
 
 echo
 echo "Done!"
---
+++ scripts/bootstrap.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+set -e
+
+echo "==============================================================="
+echo " Chrispyware Workstation Bootstrap"
+echo "==============================================================="
+
+sudo pacman -Syu --needed \
+  git \
+  openssh \
+  zsh \
+  sudo \
+  nano \
+  xfce4-terminal \
+  xorg \
+  xorg-xinit \
+  i3-wm \
+  i3status \
+  dmenu \
+  networkmanager \
+  man-db \
+  man-pages \
+  wget \
+  curl \
+  unzip \
+  zip \
+  htop \
+  btop \
+  tree \
+  ripgrep \
+  fzf \
+  ttf-jetbrains-mono-nerd
